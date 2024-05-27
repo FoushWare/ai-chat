9255768e0c319152ed0a20aedd3f36ce8264ad27
ahmed fouad <a.fouadprogrammer@gmail.com>
2024-05-27 22:19:18 +0300

    apply montai font and color brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'monta-purple': '#9b45f1',
+      },
+    },
   },
   plugins: [],
 }
